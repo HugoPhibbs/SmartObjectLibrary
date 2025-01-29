a3563c0c04339b6dbd4ad21399e230547cd02d65
Hugo Phibbs <hugophibbs1@gmail.com>
2025-01-29 13:43:51 +1300

    now populates opensearch when started

--- src/utils/opensearch_start.py
+++ src/utils/opensearch_start.py
@@ -1,6 +1,8 @@
 import subprocess
+import src.utils.upload_beams as upload_beams
 
 COMPOSE_FILE = r"C:\Users\hugop\Documents\Work\SteelProductLibrary\docker-compose.yml"
+
 
 
 def start_opensearch():
@@ -11,4 +13,8 @@ def stop_opensearch():
     subprocess.run(f"docker compose -f {COMPOSE_FILE} down", shell=True)
 
 if __name__ == "__main__":
+    stop_opensearch()
     start_opensearch()
+
+    upload_beams.main()
+
