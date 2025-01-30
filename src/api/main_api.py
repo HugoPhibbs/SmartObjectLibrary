04848b55887867487ab582ef598f7d8d1e906674
Hugo Phibbs <hugophibbs1@gmail.com>
2025-01-30 15:51:51 +1300

    upgrading api points to return ifc files

--- src/api/main_api.py
+++ src/api/main_api.py
@@ -1,5 +1,9 @@
-from flask import Flask, request, jsonify
+import io
+import os
+
+from flask import Flask, request, jsonify, send_file
 import src.core.query_engine as engine
+import zipfile
 
 app = Flask(__name__)
 
@@ -15,10 +19,14 @@ def create_object():
 def get_object(object_id: int):
     response_format = request.args.get("format", default="json", type=str)
     found_object = engine.get_by_id(object_id)
+
+    print(found_object)
     if response_format == "json":
         return jsonify(found_object)
+    elif response_format == "ifc":
+        return send_file(found_object["ifc_file_path"], as_attachment=True)
     else:
-        pass  # TODO return IFC file
+        pass  # TODO handle this
 
 
 @app.route("/object/<object_id>", methods=['DELETE'])
@@ -34,6 +42,21 @@ def update_object(object_id: int):
     return "Object updated"
 
 
+def send_objects_as_zip(objects):
+    zip_buffer = io.BytesIO()
+
+    with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+        for object in objects:
+            file_path = object["ifc_file_path"]
+            if os.path.exists(file_path):
+                zip_file.write(file_path, arcname=os.path.basename(file_path))
+
+    zip_buffer.seek(0)
+
+    # Send the zip file to the client
+    return send_file(zip_buffer, as_attachment=True, download_name="files.zip", mimetype="application/zip")
+
+
 @app.route("/object/filter", methods=['GET'])
 def get_object_by_filter():
     response_format = request.args.get("format", default="json", type=str)
@@ -41,8 +64,10 @@ def get_object_by_filter():
     found_objects = engine.get_by_filter(object_filter)
     if response_format == "json":
         return jsonify(found_objects)
+    elif response_format == "zip":
+        send_objects_as_zip(found_objects)
     else:
-        pass  # TODO return IFC files
+        pass  # TODO handle this
 
 
 @app.route("/object/nlp", methods=['GET'])
@@ -57,4 +82,4 @@ def get_object_by_nlp():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
