5bd181d5d6a979d7fb952ec4acac001809399c21
Hugo Phibbs <hugophibbs1@gmail.com>
2025-01-31 16:44:42 +1300

    implementing more methods in query engine

--- src/core/query_engine.py
+++ src/core/query_engine.py
@@ -1,11 +1,37 @@
 from typing import List
 
-from scripts.opensearch_client import client
+import ifcopenshell
+
+from core.opensearch_client import client
+from src.core.ifc_to_json import ifc_file_to_object_dict
+from src.core.FileStore import FileStore
+
+file_store = FileStore()
 
 
-def get_by_id(object_id, format="ifc") -> dict:
-    response = client.get(index="objects", id=object_id)
-    return response["_source"]
+def get_by_id(object_id, format="ifc") -> dict | str:
+    if format == "json":
+        response = client.get(index="objects", id=object_id)
+        return response["_source"]
+
+    elif format == "ifc":
+        ifc_file_path = file_store.object_ifc_path(object_id)
+        return ifc_file_path
+
+
+def get_all_objects(format="ifc") -> List[dict] | str:
+    if format == "json":
+        response = client.search(index="objects", body={"query": {"match_all": {}}})
+        return response["hits"]["hits"]
+
+    elif format == "ifc":
+        objects = []
+        for hit in get_all_objects("json"):
+            object_id = hit["_id"]
+            ifc_file_path = file_store.object_ifc_path(object_id)
+            objects.append(ifc_file_path)
+
+        return objects
 
 
 def get_by_filter(query_filter) -> List[dict]:
@@ -18,12 +44,28 @@ def get_by_nlp(nlp_query) -> List[dict]:
 
 
 def delete_by_id(object_id) -> None:
-    pass
+    response = client.delete(index="objects", id=object_id)
+    file_store.delete_object_ifc(object_id)
+    return response
 
 
-def update_by_id(object_id, new_object_data):
-    return None
+def update_by_id(object_id: str, ifc_file: ifcopenshell.file) -> None:
+    object_dict, _, _ = ifc_file_to_object_dict(ifc_file)
+    response = client.update(index="objects", id=object_id, body={"doc": object_dict})
+    return response
 
 
-def create_object(new_object_data):
-    return None
+def create_object(object_ifc_file: ifcopenshell.file, ifc_type="IfcBeam"):
+    """
+    Handles creating a new object in the OpenSearch index
+
+    Assumes that the new object data is an ifc file
+
+    :param object_ifc_file: ifc file
+    :return:
+    """
+    object_dict, id, _ = ifc_file_to_object_dict(object_ifc_file, ifc_type)
+    response = client.index(index="objects", body=object_dict, id=id)
+    file_store.add_object_ifc(id, object_ifc_file)
+
+    return response
