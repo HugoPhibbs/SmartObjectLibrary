89069040d3239d08461ee8e9afdcb012997e1efb
Hugo Phibbs <hugophibbs1@gmail.com>
2025-03-13 16:40:03 +1300

    adding section type to json data
    seperate from mass per length measurement. Just so it lines up with connection tables

--- src/scripts/beams/beams_to_json.py
+++ src/scripts/beams/beams_to_json.py
@@ -19,7 +19,7 @@ def correct_mass_per_metre(object_dict):
 
 def add_section_type(object_dict):
     model = object_dict["property_sets"]["Identity Data"]["Model"]["value"]
-    pattern = r"(?P<section>\d{3,}[A-Z]{2})(?P<mass_per_length>\d+\.\d+)"
+    pattern = r"(?P<section_type>\d{3,}[A-Z]{2})(?P<mass_per_length>\d+\.\d+)"
     match = re.match(pattern, model)
     if match:
         # TODO integrate this line of code into LibraryObject.from_ifc_file
