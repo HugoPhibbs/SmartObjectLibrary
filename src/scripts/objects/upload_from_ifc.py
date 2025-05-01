705118a3746cf05fa0ae5def061d647aaded4f49
Hugo Phibbs <hugophibbs1@gmail.com>
2025-05-01 15:01:35 +1200

    renaming beams_to_json

--- src/scripts/objects/upload_from_ifc.py
+++ src/scripts/objects/upload_from_ifc.py
@@ -1,5 +1,5 @@
 import src.scripts.objects.upload_from_json as upload_beams
-import src.scripts.objects.beams_to_json as beams_to_json
+import src.scripts.objects.ifc_to_json as beams_to_json
 import src.scripts.objects.parse_building_ifc_to_single_ifc as beams_to_single_ifc
 
 if __name__ == "__main__":
