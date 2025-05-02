3cb6bdd81e31fcdb485a361b123aa12b6745582d
Hugo Phibbs <hugophibbs1@gmail.com>
2025-05-02 15:51:19 +1200

    deleting opensearch_start.py

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import threading
-from src.scripts.opensearch.opensearch_start import stop_opensearch, start_opensearch
 from src.api.main_api import app
 
-
-def run_opensearch():
-    stop_opensearch()
-    start_opensearch()
-
-
-def run_flask():
-    app.run()
-
-
 if __name__ == "__main__":
-    opensearch_thread = threading.Thread(target=run_opensearch)
-    opensearch_thread.start()
-
-    run_flask()
+    app.run()
