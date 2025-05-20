af1c4610126a22d97a9a7bb4ac334970307567bc
Hugo Phibbs <hugophibbs1@gmail.com>
2025-05-20 14:57:54 +1200

    fixing script to convert dimensions, now prints out object name aswell

--- src/scripts/objects/wsp_building/get_dimension_types.py
+++ src/scripts/objects/wsp_building/get_dimension_types.py
@@ -1,5 +1,5 @@
 import os
-from src.core.LibraryObject import LibraryObject
+from src.core.LibraryObject import LibraryObject as LibraryObjectV2
 import ifcopenshell
 
 
@@ -8,7 +8,7 @@ ifc_dir = r"C:\Users\hugop\Documents\Work\SmartObjectLibrary\data\objects\ifc"
 
 
 if __name__ == "__main__":
-    unique_dim_dicts = set()
+    unique_dim_dicts = {}
 
     for file in os.listdir(ifc_dir):
         if file.startswith(s_t_prefix):
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
         file = ifcopenshell.open(file_path)
 
-        this_object, file_name = LibraryObject.from_ifc_file(file)
+        this_object, file_name = LibraryObjectV2.from_ifc_file(file)
 
         this_dimensions_keys = list(this_object.property_sets["Dimensions"].keys())
 
@@ -26,11 +26,15 @@ if __name__ == "__main__":
 
         this_dimensions_keys = tuple(this_dimensions_keys)
 
+        name_parsed = ":".join(this_object.identity_data.primary_info.name.split(":")[:-1]) # Remove ID
+
         if this_dimensions_keys not in unique_dim_dicts:
-            unique_dim_dicts.add(this_dimensions_keys)
+            unique_dim_dicts[this_dimensions_keys] = name_parsed
+
+    unique_dim_dicts_list = [(k, v) for k, v in unique_dim_dicts.items()]
 
     print("Unique dimension types:")
-    for dim in unique_dim_dicts:
-        print(dim)
+    for dim in unique_dim_dicts_list:
+        print(f"{dim[1]}: Dims: {dim[0]}")
 
 
