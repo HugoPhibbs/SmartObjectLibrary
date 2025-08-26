aed0be9ad5ccd2bdecfbf4255d0436c0c8b46330
Hugo Phibbs <hugophibbs1@gmail.com>
2025-08-26 17:30:05 +1200

    moving arg parser from main_api to main
    adding an arg to upload to opensearch also when running

--- src/site/api/main_api.py
+++ src/site/api/main_api.py
@@ -1,14 +1,8 @@
 from flask import Flask
 from src.site.api.routers.object_router import object_bp
 from src.site.api.routers.connection_router import connection_bp
-import argparse
+
 from flask_cors import CORS
-
-parser = argparse.ArgumentParser(description="Run the Flask API server.")
-
-parser.add_argument("--host", type=str, default="127.0.0.1")
-parser.add_argument("--port", type=int, default=5000)
-args = parser.parse_args()
 
 app = Flask(__name__)
 CORS(app)
@@ -17,4 +11,4 @@ app.register_blueprint(object_bp, url_prefix='/object')
 app.register_blueprint(connection_bp, url_prefix='/connection')
 
 if __name__ == '__main__':
-    app.run(host=args.host, port=args.port, debug=True)
+    app.run(debug=True)
