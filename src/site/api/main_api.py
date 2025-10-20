927b0307237e8f9ead10f54c67a7d68ca361cc73
Hugo Phibbs <hugophibbs1@gmail.com>
2025-10-20 16:19:46 +1300

    adding auth to main api

--- src/site/api/main_api.py
+++ src/site/api/main_api.py
@@ -1,38 +1,57 @@
-from flask import Flask, request, jsonify
+import datetime
+
+from flask import Flask, request
 from src.site.api.routers.object_router import object_bp
 from src.site.api.routers.connection_router import connection_bp
+from src.site.api.routers.auth_router import auth_bp
+from src.site.user_store import get_username
+from src.site.user_store import User
 
 from flask_cors import CORS
-from flask_httpauth import HTTPTokenAuth
+from flask_login import LoginManager
+
 from dotenv import load_dotenv
 import os
-import hmac
+from datetime import timedelta
 
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+app.secret_key = os.getenv("SESSION_SECRET_KEY")
 
-auth = HTTPTokenAuth(scheme="Bearer")
 
-app.register_blueprint(object_bp, url_prefix='/object')
-app.register_blueprint(connection_bp, url_prefix='/connection')
+app.config.update(
+    SESSION_COOKIE_SAMESITE="None",   # allow cross-site
+    SESSION_COOKIE_SECURE=True,       # must be HTTPS in prod
+    SESSION_COOKIE_HTTPONLY=True,
+    PERMANENT_SESSION_LIFETIME=timedelta(days=3),
+    SESSION_REFRESH_EACH_REQUEST=True
+)
+
+CORS(app, supports_credentials=True, origins=[os.getenv("FRONT_END_DOMAIN")])
+
+# auth = HTTPTokenAuth(scheme="Bearer")
+
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id: id):
+    print(f"id: {user_id}")
+    username = get_username(str(user_id))
+    if username:
+        return User(user_id, username)
+    return None
+
+
+app.register_blueprint(object_bp, url_prefix='/api/object')
+app.register_blueprint(connection_bp, url_prefix='/api/connection')
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 
 @app.before_request
 def log_request_info():
     print(f"{request.method} {request.path}")
-
-
-@auth.verify_token
-def verify_token(token):
-    # Implement your token verification logic here
-    return token if hmac.compare_digest(token, os.getenv("API_TOKEN") or "") else None
-
-
-@app.errorhandler(401)
-def _unauthorized(e):
-    return jsonify(error="Unauthorized"), 401
 
 
 if __name__ == '__main__':
